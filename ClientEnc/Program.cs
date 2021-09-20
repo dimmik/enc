ac10c057efb006c6eb463494e07154cb10e2fe1f
ad@dimmik.org <ad@dimmik.org>
2021-09-20 15:29:13 +0300

    add singleton encryptor

--- ClientEnc/Program.cs
+++ ClientEnc/Program.cs
@@ -1,3 +1,4 @@
+using ClientEnc.Algorithms;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,8 @@ namespace ClientEnc
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSingleton<ISymmetricEncryptor>(new Aes256Encryptor());
 
             await builder.Build().RunAsync();
         }
